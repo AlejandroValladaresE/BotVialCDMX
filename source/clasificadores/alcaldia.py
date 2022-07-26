4c27923154832e3ba38fe7bd5436494d865f927c
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2022-07-25 20:11:21 -0500

    [Clasificadores] Usar unidecode para quitar diacriticas/acentos

--- source/clasificadores/alcaldia.py
+++ source/clasificadores/alcaldia.py
@@ -4,6 +4,7 @@ import re
 
 from enum import Enum
 from typing import Dict, Optional
+from unidecode import unidecode
 
 class Alcaldía(Enum):
     Azcapotzalco       = "AZC"
@@ -30,16 +31,17 @@ _csv_alcaldias_ubicacion: str = os.environ.get("CSV_ALCALDIAS", "recursos/alcald
 with open(_csv_alcaldias_ubicacion) as archivo:
     lector = csv.DictReader(archivo)
     for fila in lector:
-        alcaldias[fila["texto"].lower()] = Alcaldía(fila["alcaldía"])
+        alcaldias[unidecode(fila["texto"].lower())] = Alcaldía(fila["alcaldía"])
 
 def identifica_alcaldia(texto) -> Optional[Alcaldía]:
     '''
     Función para identificar si dentro del texto del tuit se reconoce la alcaldia
     donde se genero el evento.
     '''
+    texto_limpio = unidecode(texto.lower())
 
     # Es posible que haya más de un texto de alcaldía
-    posibles_alcaldias = set([ alcaldias[clave] for clave in alcaldias.keys() if re.search(clave, texto.lower()) ])
+    posibles_alcaldias = set([ alcaldias[clave] for clave in alcaldias.keys() if re.search(clave, texto_limpio) ])
 
     # Solo regresa cuando hay una sola posible alcaldía
     if(len(posibles_alcaldias) == 1):
